871c4e09822cc70b6957b156deb82051d62808cf
shuhan <1552257473@qq.com>
2022-08-02 17:07:01 +0800

    输入提交信息

---
+++ src/main/java/com/han/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.han.git;
+
+/**
+ * @PACKAGE_NAME: com.han.git
+ * @Author XSH
+ * @Date 2022-08-02 16:58
+ * @Version 1.0
+ * 描述：
+ **/
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("测试，git");
+    }
+}
